3d8440c64fd4911a85b26a74c901d5ad0586d1b5
Spencer Will <spencer.a.will@gmail.com>
2026-01-22 14:45:14 -0500

    create reMIND watch app

---
+++ apple/reMIND/reMIND Watch App/reMINDApp.swift
@@ -0,0 +1,17 @@
+//
+//  reMINDApp.swift
+//  reMIND Watch App
+//
+//  Created by Spencer Will on 1/22/26.
+//
+
+import SwiftUI
+
+@main
+struct reMIND_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ apple/reMIND/reMIND Watch AppTests/reMIND_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  reMIND_Watch_AppTests.swift
+//  reMIND Watch AppTests
+//
+//  Created by Spencer Will on 1/22/26.
+//
+
+import Testing
+@testable import reMIND_Watch_App
+
+struct reMIND_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
